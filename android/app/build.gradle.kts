7e76430e0c4f35409d533f0468f4cf84ddfddcd6
sabbirahmed6amtech <syedsabbirahmed.6amtech@gmail.com>
2026-01-04 17:48:58 +0600

    feat : setup firebase and receive foreground message

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
-
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
 android {
     namespace = "com.example.sixvalley"
     compileSdk = flutter.compileSdkVersion
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:sixvalley/feature/splash/view/splash_screen.dart';
 import 'package:sixvalley/helper/dependency_injection.dart' as di;
 import 'package:sixvalley/theme/light_theme.dart';
 import 'package:sixvalley/theme/dark_theam.dart';
 import 'package:sixvalley/utils/app_constants.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseMessaging.instance.requestPermission();
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+  print('FCM Token: $fcmToken');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Foreground message received');
+    print(message.notification?.title);
+    print(message.notification?.body);
+  });
+
   await di.init();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
